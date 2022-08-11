b2aa6350b854796f2733b5938e452a13f6098f36
kagenmi <109903144+kagenmi@users.noreply.github.com>
2022-08-11 23:57:37 +0900

    feat(nvim): mapping for closing all buffers

--- nvchad/custom/mappings.lua
+++ nvchad/custom/mappings.lua
@@ -1,39 +1,26 @@
 local M = {}
 
-M.hello = {
-   -- NOTE: In the official config, the 2nd parameter is missing.
+M.general = {
    n = {
-      ["j"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', "", opts = { expr = true } },
-      ["k"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', "", opts = { expr = true } },
-      ["<Up>"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', opts = { expr = true } },
-      ["<Down>"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', opts = { expr = true } },
+      ['<leader>cc'] = { "<cmd> bufdo q <CR>", "close all buffers" },
    },
    v = {
-      ["j"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', "", opts = { expr = true } },
-      ["k"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', "", opts = { expr = true } },
-      ["<Up>"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', "", opts = { expr = true } },
-      ["<Down>"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', "", opts = { expr = true } },
-
       ['<'] = { "<gv", "Indent backward" },
       ['>'] = { ">gv", "Indent forward" },
-
-      -- Don't copy after pasting
-      ["p"] = { 'p:let @+=@0<CR>:let @"=@0<CR>', "", opts = { silent = true } },
    },
-
 }
 
 M.truzen = {
    n = {
-      ["<leader>ta"] = { "<cmd> TZAtaraxis <CR>", "   truzen ataraxis" },
-      ["<leader>tm"] = { "<cmd> TZMinimalist <CR>", "   truzen minimal" },
-      ["<leader>tf"] = { "<cmd> TZFocus <CR>", "   truzen focus" },
+      ["<leader>ta"] = { "<cmd> TZAtaraxis <CR>", "truzen ataraxis" },
+      ["<leader>tm"] = { "<cmd> TZMinimalist <CR>", "truzen minimal" },
+      ["<leader>tf"] = { "<cmd> TZFocus <CR>", "truzen focus" },
    },
 }
 
 M.treesitter = {
    n = {
-      ["<leader>cu"] = { "<cmd> TSCaptureUnderCursor <CR>", "  find media" },
+      ["<leader>cu"] = { "<cmd> TSCaptureUnderCursor <CR>", "find media" },
    },
 }
 
@@ -44,7 +31,7 @@ M.shade = {
             require("shade").toggle()
          end,
 
-         "   toggle shade.nvim",
+         "toggle shade.nvim",
       },
    },
 }
@@ -63,22 +50,22 @@ M.comment = {
             require("Comment.api").toggle_current_linewise()
          end,
 
-         "蘒  toggle comment",
+         "toggle comment",
       },
    },
    v = {
       ["<leader>/"] = "",
       ["<C-_>"] = {
          "<ESC><cmd>lua require('Comment.api').toggle_linewise_op(vim.fn.visualmode())<CR>",
-         "蘒  toggle comment",
+         "toggle comment",
       },
    },
 }
 
 M.tabufline = {
    n = {
-      ["<TAB>"] = { "<cmd> tabprevious <CR>", "  goto next tab" },
-      ["<S-TAB>"] = { "<cmd> tabnext <CR> ", "  goto prev tab" }
+      ["<TAB>"] = { "<cmd> tabprevious <CR>", "goto next tab" },
+      ["<S-TAB>"] = { "<cmd> tabnext <CR> ", " goto prev tab" }
    }
 }
 
@@ -92,7 +79,7 @@ M.quickrun = {
    n = {
       ["<leader>qr"] = {
          ":QuickRun<CR>",
-         "  Quick Run",
+         "Quick Run",
       },
    },
 }
@@ -101,11 +88,11 @@ M.winshift = {
    n = {
       ["<C-w>m"] = {
          ":WinShift<CR>",
-         "侀Enter Win-Move mode",
+         "Enter Win-Move mode",
       },
       ["<C-w>x"] = {
          ":WinShift swap<CR>",
-         "侀Swap windows",
+         "Swap windows",
       },
    },
 }
