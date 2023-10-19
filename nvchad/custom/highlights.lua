fe694dd89b5956841191c8c93f0ab808afe534d6
kagenmi <109903144+kagenmi@users.noreply.github.com>
2023-10-19 18:15:16 +0900

    [nvim] update nvchad

---
+++ nvchad/custom/highlights.lua
@@ -0,0 +1,32 @@
+-- To find any highlight groups: "<cmd> Telescope highlights"
+-- Each highlight group can take a table with variables fg, bg, bold, italic, etc
+-- base30 variable names can also be used as colors
+
+local M = {}
+
+---@type Base46HLGroupsList
+M.override = {
+  Comment = {
+    italic = true,
+  },
+}
+
+---@type HLTable
+M.add = {
+  NvimTreeOpenedFolderName = { fg = "green", bold = true },
+  ExtraWhitespace = { bg = "red" },
+
+  -- bufferline
+  TbLineBufOn = { fg = 'white', bg = 'one_bg3', bold = true },
+}
+
+M.changed_themes = {
+  tokyonight = {
+    base_30 = {
+      -- default is too dark
+      grey_fg = '#8b93b6',
+    }
+  }
+}
+
+return M
