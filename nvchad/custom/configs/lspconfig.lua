e04a0c147477233c2b2c287e7c3a8ba619f95636
kagenmi <109903144+kagenmi@users.noreply.github.com>
2023-12-23 05:14:36 +0900

    nvim: add pyright config

--- nvchad/custom/configs/lspconfig.lua
+++ nvchad/custom/configs/lspconfig.lua
@@ -2,6 +2,9 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
+local lsputil = require "lspconfig/util"
+
+local utils = require "custom.utils"
 
 -- if you just want default config for the servers then put them in a table
 local servers = { "html", "cssls", "tsserver", "clangd" }
@@ -13,5 +16,26 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- 
--- lspconfig.pyright.setup { blabla}
+lspconfig.pyright.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  root_dir = lsputil.root_pattern { "pyproject.toml", ".git" },
+  settings = {
+    python = {
+      analysis = {
+        diagnosticSeverityOverrides = {
+          reportUnboundVariable = 'none',
+          reportOptionalIterable = 'none',
+          reportOptionalSubscript = 'none',
+          reportGeneralTypeIssues = 'none',
+        },
+        authSearchPath = true,
+        diagnosticMode = "workspace",
+        useLibraryCodeForTypes = true,
+      },
+    },
+  },
+  on_init = function(client)
+    client.config.settings.python.pythonPath = utils.get_python_path(client.config.root_dir)
+  end,
+}
