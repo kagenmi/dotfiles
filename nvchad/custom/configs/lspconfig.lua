8c8d9eead271044a8a611a8ccee54bbe021e294e
kagenmi <109903144+kagenmi@users.noreply.github.com>
2024-02-05 00:20:40 +0000

    [nvim] add lsp terraform-ls

--- nvchad/custom/configs/lspconfig.lua
+++ nvchad/custom/configs/lspconfig.lua
@@ -7,7 +7,7 @@ local lsputil = require "lspconfig/util"
 local utils = require "custom.utils"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "gopls", "pyright" }
+local servers = { "html", "cssls", "tsserver", "clangd", "gopls", "pyright", "terraform-ls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
