91e49eaeb399933b08ee9f6e58386ab59ba2b15a
kagenmi <109903144+kagenmi@users.noreply.github.com>
2024-04-08 01:17:41 +0000

    [nvim] add plugin 'flash.nvim'

--- nvchad/custom/plugins.lua
+++ nvchad/custom/plugins.lua
@@ -97,7 +97,21 @@ local plugins = {
     "Vonr/align.nvim",
     branch = "v2",
     lazy = true,
-  }
+  },
+
+  -- Enhance f,t,F,T motions
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
 
   -- general-purpose motion plugin
   -- {
