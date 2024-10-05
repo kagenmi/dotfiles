f89bb267861fab1883acafc17695853508c104cb
kagenmi <109903144+kagenmi@users.noreply.github.com>
2024-10-05 19:42:53 +0900

    [nvim] feat: add typescript-script

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -47,9 +47,16 @@ local server_opts = {
       hostInfo = 'neovim',
       preferences = {
         quotePreference = 'single',
-      }
-    }
-  }
+      },
+      plugins = {
+        {
+          name = "@vue/typescript-plugin",
+          location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
+          languages = {"javascript", "typescript", "vue"},
+        },
+      },
+    },
+  },
 }
 
 local function setup()
