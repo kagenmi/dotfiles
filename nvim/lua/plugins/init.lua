127f8744328a3b6cdffb50d2396fbddaf98c5c18
kagenmi <109903144+kagenmi@users.noreply.github.com>
2024-11-07 20:45:25 +0900

    [nvim]feat: uninstall better-escape

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -67,13 +67,13 @@ return {
   -- Install custom plugins
   -- ----------------------
 
-  {
-    "max397574/better-escape.nvim",
-    event = "InsertEnter",
-    config = function()
-      require("better_escape").setup()
-    end,
-  },
+  -- {
+  --   "max397574/better-escape.nvim",
+  --   event = "InsertEnter",
+  --   config = function()
+  --     require("better_escape").setup()
+  --   end,
+  -- },
 
   {
     "windwp/nvim-ts-autotag",
