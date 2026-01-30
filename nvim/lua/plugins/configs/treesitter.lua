dfa138cc27be62d7fcd556bc509d68c0ffe665dc
kagenmi <109903144+kagenmi@users.noreply.github.com>
2026-01-30 17:13:18 +0900

    [nvim] Cleanup treesitter configuration
    - Remove custom bicep parser setup
    - Simplify configuration structure
    - Remove unused setup function

--- nvim/lua/plugins/configs/treesitter.lua
+++ nvim/lua/plugins/configs/treesitter.lua
@@ -1,5 +1,4 @@
 return {
-  opts = {
   ensure_installed = {
     "vim",
     "vimdoc",
@@ -22,17 +21,4 @@ return {
     --   "python"
     -- },
   },
-
-  },
-  setup = function()
-    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-    parser_config.bicep = {
-      install_info = {
-        url = 'https://github.com/tree-sitter-grammars/tree-sitter-bicep',
-        files = { 'src/parser.c', 'src/scanner.c' },
-        branch = 'main',
-      },
-      filetype = 'bicep',
-    }
-  end,
 }
